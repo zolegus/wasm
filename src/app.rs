6536d2cbd6b52c316190c14369069ef43db3958e
zolegus <zolegus@gmail.com>
2024-08-08 00:46:36 +0200

    refact: canvas and virtual keyboard focus for mobile web

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,7 @@
+use egui::{
+    introspection::{font_family_ui, font_id_ui},
+    Widget,
+};
 use log::info;
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -5,6 +9,7 @@ use log::info;
 pub struct MyApp {
     label: String,
     text: String,
+    font_id: egui::FontId,
 }
 
 impl Default for MyApp {
@@ -12,6 +17,7 @@ impl Default for MyApp {
         Self {
             label: "Hello WASM!".to_owned(),
             text: String::from(""),
+            font_id: egui::FontId::proportional(18.0),
         }
     }
 }
@@ -36,12 +42,21 @@ impl eframe::App for MyApp {
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("test");
+            egui::introspection::font_id_ui(ui, &mut self.font_id);
+            ui.label(egui::RichText::new("With formatting").font(self.font_id.clone()));
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.add(egui::TextEdit::singleline(&mut self.label).desired_width(120.0));
+                ui.label(egui::RichText::new("Write something: ").font(self.font_id.clone()));
+                ui.add(
+                    egui::TextEdit::singleline(&mut self.label)
+                        .desired_width(10.0 * self.font_id.clone().size)
+                        .font(self.font_id.clone()),
+                );
             });
-            ui.add(egui::TextEdit::multiline(&mut self.text).hint_text("Type something!"));
+            ui.add(
+                egui::TextEdit::multiline(&mut self.text)
+                    .hint_text("Type something!")
+                    .font(self.font_id.clone()),
+            );
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 egui::warn_if_debug_build(ui);
